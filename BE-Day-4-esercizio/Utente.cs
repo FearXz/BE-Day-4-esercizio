2d007c1b14d01e1fe7734a36869b2e8570d7710f
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2024-02-01 16:46:58 +0100

    ok con registrazione

--- BE-Day-4-esercizio/Utente.cs
+++ BE-Day-4-esercizio/Utente.cs
@@ -25,21 +25,22 @@ namespace BE_Day_4_esercizio
         public static string Username { get; set; }
         public static string Password { get; set; }
         public static bool IsLoggedIn { get; set; }
-        public static List<DateTime> LoginDate { get; set; } = new List<DateTime>();
-        public static DateTime LastLoginTime { get; private set; }
+        public static List<(string, string)> UserList { get; set; } = new List<(string, string)>();
+        public static List<string> LoginDate { get; set; } = new List<string>();
+        public static DateTime LastLoginTime { get; set; }
 
-
+        // Menu () Genera un menu con le operazioni disponibili e richiama il metodo corrispondente alla scelta dell'utente 
         public static void Menu()
         {
 
-            Console.WriteLine("===============OPERAZIONI==============");
+            Console.WriteLine("\n===============OPERAZIONI==============");
             Console.WriteLine("Scegli l'operazione da effettuare:");
             Console.WriteLine("1.: Login");
             Console.WriteLine("2.: Logout");
             Console.WriteLine("3.: Verifica ora e data di login");
             Console.WriteLine("4.: Lista degli accessi");
             Console.WriteLine("5.: Esci");
-            Console.WriteLine("========================================");
+            Console.WriteLine("========================================\n");
             string scelta = Console.ReadLine();
             switch (scelta)
             {
@@ -64,73 +65,118 @@ namespace BE_Day_4_esercizio
                     break;
             }
         }
-
+        /*
+         * Login () Chiede all'utente di inserire username e password e verifica se l'utente è già registrato,
+         * se non lo è lo registra e lo logga, se lo è controlla che la password inserita sia corretta e lo logga
+         */
         public static void Login()
-        {
+        {   // Chiede all'utente di inserire username e password
             Console.WriteLine("Inserisci username:");
-            Username = Console.ReadLine();
+            string inputUsername = Console.ReadLine();
             Console.WriteLine("Inserisci password:");
-            Password = Console.ReadLine();
+            string inputPassword = Console.ReadLine();
             Console.WriteLine("Conferma password:");
             string confirmPassword = Console.ReadLine();
-            if (Username != null && Password != null && Password == confirmPassword)
+
+            // Verifica che username e password non siano nulli e che la password sia uguale alla conferma password
+            if (inputUsername != null && inputPassword != null && inputPassword == confirmPassword)
             {
-                IsLoggedIn = true;
-                LastLoginTime = DateTime.Now;
-                LoginDate.Add(LastLoginTime);
-                Console.WriteLine("Login effettuato con successo!");
+                // Cerca l'utente nella lista
+                var user = UserList.Find(u => u.Item1 == inputUsername);
+
+                // Verifica se l'utente è già registrato
+                if (user.Item1 != null && user.Item2 != null)
+                {
+                    /* 
+                     * Verifica che la password inserita sia corretta
+                     * e se la password è corretta logga l'utente
+                    */
+                    if (user.Item2 == inputPassword)
+                    {
+                        //Setta gli stati IsLoggedIn e Date
+                        SetLogin(inputUsername, inputPassword);
+                        Console.WriteLine("\nLogin effettuato con successo!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("\nPassword errata!");
+                    }
+                }
+                // Se l'utente non è registrato lo registra e lo logga
+                else
+                {
+                    UserList.Add((inputUsername, inputPassword));
+                    SetLogin(inputUsername, inputPassword);
+                    Console.WriteLine("\nUtente creato e login effettuato con successo!");
+                }
             }
             else
             {
-                Console.WriteLine("Login fallito!");
+                Console.WriteLine("\nLogin fallito!");
             }
             Menu();
         }
+
+
+        // setLogin () Setta gli stati IsLoggedIn e Date e aggiunge l'accesso alla lista degli accessi
+        public static void SetLogin(string inputUsername, string inputPassword)
+        {
+            Username = inputUsername;
+            Password = inputPassword;
+            IsLoggedIn = true;
+            LastLoginTime = DateTime.Now;
+            string lastLoginTime = LastLoginTime.ToString();
+            LoginDate.Add($"\nL'utente {inputUsername} ha effettuato il login : {lastLoginTime}");
+        }
+
+        // Logout () Effettua il logout dell'utente
         public static void Logout()
         {
             if (IsLoggedIn)
             {
                 IsLoggedIn = false;
-                Console.WriteLine("Logout effettuato con successo!");
+                Console.WriteLine("\nLogout effettuato con successo!");
             }
             else
             {
-                Console.WriteLine("Logout fallito!");
+                Console.WriteLine("\nLogout fallito!");
             }
             Menu();
         }
+
+
+
+        // CheckLogin () Verifica se l'utente è loggato e in caso affermativo stampa la data e l'ora dell'ultimo login
         public static void CheckLogin()
         {
             if (IsLoggedIn)
             {
-                Console.WriteLine($"Ultimo login effettuato  il {LastLoginTime}");
+                Console.WriteLine($"\nUltimo login effettuato  il {LastLoginTime}");
             }
             else
             {
-                Console.WriteLine("Nessun utente loggato!");
+                Console.WriteLine("\nNon sei loggato!");
             }
             Menu();
         }
+
+
+
+        // LoginList () Stampa la lista degli accessi
         public static void LoginList()
         {
             if (IsLoggedIn)
             {
                 foreach (var item in LoginDate)
                 {
-                    Console.WriteLine($"Login effettuato il {item}");
+                    Console.WriteLine($"{item}");
                 }
             }
             else
             {
-                Console.WriteLine("Nessun utente loggato!");
+                Console.WriteLine("\nNessun utente loggato!");
             }
             Menu();
         }
-
-
-
-
-
     }
-
 }
